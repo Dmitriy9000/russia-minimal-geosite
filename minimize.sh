9f214c466f8384bf1b848118fa07446caef2012f
Dmitriy <d.koudinov@gmail.com>
2026-03-12 14:14:57 +0300

    Geosite updates

--- minimize.sh
+++ minimize.sh
@@ -23,6 +23,20 @@ echo "=== Copying only important to us sections"
 cp ./extracted/geosite_ru-blocked.txt ./categories/ru-blocked
 cp ./extracted/geosite_ru-available-only-inside.txt ./categories/ru-available-only-inside
 cp ./extracted/geosite_category-ads-all.txt ./categories/category-ads-all
+cp ./extracted/geosite_apple.txt ./categories/apple
+cp ./extracted/geosite_microsoft.txt ./categories/microsoft
+cp ./extracted/geosite_steam.txt ./categories/steam
+cp ./extracted/geosite_category-games.txt ./categories/category-games
+cp ./extracted/geosite_category-ru.txt ./categories/category-ru
+cp ./extracted/geosite_reddit.txt ./categories/reddit
+cp ./extracted/geosite_category-ai-!cn.txt ./categories/category-ai-!cn
+cp ./extracted/geosite_category-ai-chat-!cn.txt ./categories/category-ai-chat-!cn
+cp ./extracted/geosite_category-cryptocurrency.txt ./categories/category-cryptocurrency
+cp ./extracted/geosite_cursor.txt ./categories/cursor
+cp ./extracted/geosite_forza.txt ./categories/forza
+cp ./extracted/geosite_piratebay.txt ./categories/piratebay
+cp ./extracted/geosite_roblox.txt ./categories/roblox
+cp ./extracted/geosite_rutracker.txt ./categories/rutracker
 
 echo "=== Enriching with extra domains"
 ./append-enrich-to-categories.sh
